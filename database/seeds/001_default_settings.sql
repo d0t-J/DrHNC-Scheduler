5a31ffe3a049e89b8aa1db09408e3d956e66dfe1
d0t-J <j.ahmed.ja7a7@gmail.com>
2026-06-17 21:57:42 +0500

    added default settings seed for consultation

---
+++ database/seeds/001_default_settings.sql
@@ -0,0 +1,20 @@
+INSERT INTO settings (
+    id,
+    consultation_start_time,
+    consultation_end_time,
+    slot_duration_minutes,
+    daily_patient_limit,
+    refund_policy,
+    cancellation_policy,
+    reschedule_policy
+)
+VALUES (
+    1,
+    '11:00',
+    '14:30',
+    20,
+    12,
+    'TBD',
+    'TBD',
+    'TBD'
+);
